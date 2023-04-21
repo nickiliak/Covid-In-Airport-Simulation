e54961b9326099e6df80875b65344f8e7775d833
Nikos Iliakis <73002179+NikolasIliakis@users.noreply.github.com>
2023-04-21 16:12:32 +0300

    Project Files

---
+++ Assets/Camera/PlayerCamera.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{   
+    public float sensX;
+    public float sensY;
+
+    public Transform orientation;
+
+    float xRotation = 0;
+    float yRotation = 0;
+    
+    private void Start() {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensX;
+
+        yRotation += mouseX;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0f);
+        orientation.rotation = Quaternion.Euler(0f, yRotation, 0f);
+    }
+}
---
+++ Assets/Scripts/Crowd.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Crowd : MonoBehaviour
+{
+    public NavMeshAgent navMeshAgent;
+    public GameObject Target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FindeTarget();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void FindeTarget()
+    {
+        navMeshAgent.destination = Target.transform.position;
+    }
+}
