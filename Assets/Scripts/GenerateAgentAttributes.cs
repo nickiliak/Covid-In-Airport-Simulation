06c6e6c94c36187cbab847e3f2f95e148517cc30
Nikos Iliakis <73002179+NikolasIliakis@users.noreply.github.com>
2023-04-28 14:47:30 +0300

    Improved Crowd Simulation

---
+++ Assets/Scripts/GenerateAgentAttributes.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class GenerateAgentAttributes : MonoBehaviour
+{
+    //Attributes
+    [SerializeField] int Age;
+    [SerializeField] int Gender;
+    [SerializeField] float Speed;
+    [SerializeField] float Accelaration;
+    [SerializeField] float Proximity;
+
+    public NavMeshAgent navMeshAgent;
+    //Health status
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Speed = Random.Range(4f, 8f);
+        Accelaration = Random.Range(4f, 10f);
+        Proximity = Random.Range(0.5f, 2f);
+
+        navMeshAgent.speed = Speed;
+        navMeshAgent.acceleration = Accelaration;  
+        navMeshAgent.radius= Proximity;
+    }
+
+}
